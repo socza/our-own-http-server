a6430cf4b309654320cacc019f52325b26549da5
Piotr Sociński <psocinski@gmail.com>
2018-01-30 17:11:29 +0100

    White characters removed

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ var http = require('http');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    
-    if (request.method === 'GET' && request.url === '/') {
 
-    	response.setHeader("Content-Type", "text/html; charset=utf-8");
-        fs.readFile('./index.html', function(err, data) {
-		
+	if (request.method === 'GET' && request.url === '/') {
+
+		response.setHeader("Content-Type", "text/html; charset=utf-8");
+		fs.readFile('./index.html', function(err, data) {
+
 			if (err) throw err;
 			response.write(data);
 			response.end();
-			
-	
-		});
-    
-    } else {
 
-            
+		});
+
+	} else {
+
 		response.setHeader("Content-Type", "image/jpg");
-        fs.readFile('./404.jpg', function(err, data) {
+		fs.readFile('./404.jpg', function(err, data) {
 		
 			if (err) throw err;
 			response.write(data);
 			response.end();
-			
-	
+
 		});
-    
-    }
+
+	}
 
 }).listen(8080);
